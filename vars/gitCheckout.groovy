f8400d52137cc4e95c7f7e5c2ad7630a3beccd54
Yogesh Nikam <yogesh.nikam@ars.de>
2023-04-07 20:41:33 +0200

    Added gitCheckout into library

---
+++ vars/gitCheckout.groovy
@@ -0,0 +1,7 @@
+def call(Map stageParams){
+    checkout([
+        $class: 'GitSCM',
+        branches: [[ name: stageParams.branch ]],
+        userRemoteConfigs: [[ url: stageParams.url]]
+    ])
+}
