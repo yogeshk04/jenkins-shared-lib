4b2122fc3b50644ec037f6c4d3afaa41555fdd9e
Yogesh Nikam <yogesh.nikam@ars.de>
2023-04-11 14:44:45 +0200

    Added docker build, scan & push library

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,7 +1,14 @@
-def call (String hubUser, String project, String imageTag){
+/* def call (String hubUser, String project, String imageTag){
     sh """
         docker image build -t ${hubUser}/${project} .
         docker image tag ${hubUser}/${project}:${imageTag}
         docker image tag ${hubUser}/${project}:latest
+    """
+} */
+def call(String aws_account_id, String region, String ecr_repo){
+    
+    sh """
+     docker build -t ${ecr_repo} .
+     docker tag ${ecr_repo}:latest ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repo}:latest
     """
 }
--- vars/dockerImageScan.groovy
+++ vars/dockerImageScan.groovy
@@ -1,6 +1,13 @@
-def call (String hubUser, String project, String imageTag){
+/* def call (String hubUser, String project, String imageTag){
     sh """
         trivy image docker image tag ${hubUser}/${project}:latest > scan.txt
         cat scan.txt
+    """
+} */
+
+def call(String aws_account_id, String region, String ecr_repo){
+    
+    sh """
+     docker rmi ${ecr_repo}:latest ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repo}:latest
     """
 }
