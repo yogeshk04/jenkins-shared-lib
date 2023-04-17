0f93a5b1afd4059a7567f7e5c1c6abbcb7ab204a
Yogesh Nikam <yogesh.nikam@ars.de>
2023-04-17 15:16:59 +0200

    Updated dockerhub jenkinsfile

--- vars/dockerhubBuild.groovy
+++ vars/dockerhubBuild.groovy
@@ -1,7 +1,6 @@
 def call (String hubUser, String project, String imageTag){
     sh """
-        docker image build -t ${hubUser}/${project} .
-        docker image tag ${hubUser}/${project}:${imageTag}
-        docker image tag ${hubUser}/${project}:latest
+        docker image build -t ${project} .
+        docker image tag ${project}:latest ${hubUser}/${project}:${imageTag}
     """
 }
